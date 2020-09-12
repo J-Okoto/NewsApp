f06600d03c913299b594c2aa16ac50c2c57ba048
J-Okoto <okotojr@gmail.com>
2020-09-13 02:33:09 +0300

    made first API call to return news sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,23 @@
 from flask import render_template
 from . import main
+from ..models import Sources
+from ..request import get_sources
+
 
 # Views
 @main.route('/')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+    general_news = get_sources('general')
+    business_news = get_sources('business')
+    entertainment_news = get_sources('entertainment')
+    sports_news = get_sources('sports')
+    technology_news = get_sources('technology')
+    science_news = get_sources('science')
+    health_news = get_sources('health')
+
     title = 'Welcome to Newsbytes'
     
-    return render_template('index.html'title=title)
+    return render_template('index.html'title=title,General=general_news, Business=business_news,
+                           Entertainment=entertainment_news, Sports=sports_news, Technology=technology_news,
+                           Science=science_news, Health=health_news)
