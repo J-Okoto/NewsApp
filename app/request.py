cbafabfc3454bb503a0f41348e468452ceaca273
J-Okoto <okotojr@gmail.com>
2020-09-13 02:06:08 +0300

    added search configuration in the request.py

---
+++ app/request.py
@@ -0,0 +1,14 @@
+from .models import Sources
+import urllib.request, json
+
+
+
+api_key = None
+sources_url = None
+articles_url = None
+
+def configure_request(app):
+    global api_key, base_url, articles_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['SOURCES_BASE_URL']
+    articles_url = app.config['EVERYTHING_SOURCE_BASE_URL']
