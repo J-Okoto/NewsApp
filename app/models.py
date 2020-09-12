711a5de155d18bb918790d790c2300797df89310
J-Okoto <okotojr@gmail.com>
2020-09-13 01:52:14 +0300

    added model class to define news article objects

--- app/models.py
+++ app/models.py
@@ -10,3 +10,16 @@ class Sources:
         self.url = url
         self.category = category
         self.country = country
+
+
+class Articles:
+    '''
+    Article class that defines the article objects
+    '''
+
+    def __init__(self, author, title, description, url, urlToImage):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
