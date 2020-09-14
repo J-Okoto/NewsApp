5b3e7f1b7494636b29c13eb7f44ba238cc827e7a
J-Okoto <okotojr@gmail.com>
2020-09-14 21:15:59 +0300

    added error handler

--- app/request.py
+++ app/request.py
@@ -1,17 +1,18 @@
 
 import urllib.request, json
-from .models import Sources
+from .models import Sources,Articles
 
 
 api_key = None
 base_url = None
+articles_url=None
 
 
 def configure_request(app):
     global api_key,base_url
     api_key =app.config['NEWS_API_KEY']
     base_url =app.config['SOURCES_BASE_URL']
-   
+    articles_url = app.config['EVERYTHING_SOURCE_BASE_URL']
 
 def get_sources(category):
     '''
--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
     General configuration parent class
     '''
     SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    EVERYTHING_SOURCE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&pageSize={}&apiKey={}'
+    
 
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     
